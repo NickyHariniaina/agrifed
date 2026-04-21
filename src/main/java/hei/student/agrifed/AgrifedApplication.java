351275cf65d8a6e2966888d65845ef39c7a9dc09
NickyHariniai <ferrissushi@gmail.com>
2026-04-21 15:11:19 +0300

    initial commit

---
+++ src/main/java/hei/student/agrifed/AgrifedApplication.java
@@ -0,0 +1,13 @@
+package hei.student.agrifed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrifedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrifedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hei/student/agrifed/AgrifedApplicationTests.java
@@ -0,0 +1,13 @@
+package hei.student.agrifed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrifedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
