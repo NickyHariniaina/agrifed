33b2e56a558c534f006fa356ad4ec77285c4004b
Rindra <hei.rindra.2@gmail.com>
2026-04-22 16:08:23 +0300

    feat:using getter and setter lombok

--- src/main/java/hei/student/agrifed/entity/dto/AssignIdentityDto.java
+++ src/main/java/hei/student/agrifed/entity/dto/AssignIdentityDto.java
@@ -1,13 +1,14 @@
 package hei.student.agrifed.entity.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class AssignIdentityDto {
     private Integer federationNumber;
     private String name;
 
     public AssignIdentityDto() {}
 
-    public Integer getFederationNumber() { return federationNumber; }
-    public void setFederationNumber(Integer federationNumber) { this.federationNumber = federationNumber; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
 }
