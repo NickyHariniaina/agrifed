e5ac9b440880ea0a1341fe8abad1873c7e044c8b
NickyHariniai <ferrissushi@gmail.com>
2026-04-22 08:27:50 +0300

    feat(dto): add mapper to member

--- src/main/java/hei/student/agrifed/entity/dto/CreateMemberDto.java
+++ src/main/java/hei/student/agrifed/entity/dto/CreateMemberDto.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 import hei.student.agrifed.entity.Gender;
+import hei.student.agrifed.entity.Member;
 import hei.student.agrifed.entity.MemberOccupation;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -26,4 +27,20 @@ public class CreateMemberDto {
     private List<String> referees;
     private Boolean registrationFeePaid;
     private Boolean membershipDuesPaid;
+
+    public Member toMember() {
+        Member member = new Member();
+        member.setFirstName(firstName);
+        member.setLastName(lastName);
+        member.setBirthDate(birthDate);
+        member.setGender(gender);
+        member.setAddress(address);
+        member.setPhoneNumber(phoneNumber);
+        member.setProfession(profession);
+        member.setEmail(email);
+        member.setOccupation(occupation);
+        member.setReferees(referees);
+        member.setJoinedAt(LocalDate.now());
+        return member;
+    }
 }
