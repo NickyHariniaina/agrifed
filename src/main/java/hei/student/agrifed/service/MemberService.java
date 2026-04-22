a0b38f9e1601a148eb0844530febc8f2b1ec0a60
NickyHariniai <ferrissushi@gmail.com>
2026-04-22 09:03:58 +0300

    feat: add collectivity checker into member serviec

--- src/main/java/hei/student/agrifed/service/MemberService.java
+++ src/main/java/hei/student/agrifed/service/MemberService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import hei.student.agrifed.entity.Member;
 import hei.student.agrifed.entity.dto.CreateMemberDto;
+import hei.student.agrifed.repository.CollectivityRepository;
 import hei.student.agrifed.repository.MemberRepository;
 import hei.student.agrifed.utils.MemberValidator;
 import lombok.AllArgsConstructor;
@@ -13,12 +14,14 @@ import lombok.AllArgsConstructor;
 public class MemberService {
 
     private MemberRepository memberRepository;
+    private CollectivityRepository collectivityRepository;
     private MemberValidator memberValidator;
 
     public Member save(CreateMemberDto createMemberDto) {
         Member member = createMemberDto.toMember();
         memberValidator.checkCreateMemberDto(createMemberDto);
         memberValidator.checkReferees(createMemberDto, memberRepository);
+        memberValidator.checkCollectivity(createMemberDto, collectivityRepository);
         return memberRepository.save(member);
     }
 
