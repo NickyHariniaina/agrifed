7816c52214aaaa44e933e494348537512917ede0
Rindra <hei.rindra.2@gmail.com>
2026-04-22 23:27:41 +0300

    feat: accountCreditedIdentifier

--- src/main/java/hei/student/agrifed/entity/dto/CreateMemberPaymentDto.java
+++ src/main/java/hei/student/agrifed/entity/dto/CreateMemberPaymentDto.java
@@ -5,5 +5,6 @@ import hei.student.agrifed.entity.PaymentMode;
 public class CreateMemberPaymentDto {
     private Double      amount;
     private String      membershipFeeIdentifier;
+    private String      accountCreditedIdentifier;
     private PaymentMode paymentMode;
 }
