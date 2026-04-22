20feef5e5942efb8a3e4001f83ec7225ce7dcafb
Rindra <hei.rindra.2@gmail.com>
2026-04-22 23:26:36 +0300

    feat: add CreateMemberPaymentDto

---
+++ src/main/java/hei/student/agrifed/entity/dto/CreateMemberPaymentDto.java
@@ -0,0 +1,8 @@
+package hei.student.agrifed.entity.dto;
+
+import hei.student.agrifed.entity.PaymentMode;
+
+public class CreateMemberPaymentDto {
+    private Double      amount;
+    private PaymentMode paymentMode;
+}
