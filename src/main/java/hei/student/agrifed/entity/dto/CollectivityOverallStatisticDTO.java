fd7fe28df7b9156c899f164a2c820326b51f948c
Rindra <hei.rindra.2@gmail.com>
2026-05-06 16:28:04 +0300

    feat: add DTO for collectivity overall statistics

---
+++ src/main/java/hei/student/agrifed/entity/dto/CollectivityOverallStatisticDTO.java
@@ -0,0 +1,17 @@
+package hei.student.agrifed.entity.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CollectivityOverallStatisticDTO {
+    private AssignIdentityDto collectivityInformation;
+    private Integer newMembersNumber;
+    private Double overallMemberCurrentDuePercentage;
+}
